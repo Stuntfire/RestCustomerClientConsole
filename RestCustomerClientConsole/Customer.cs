3a187ad943c0eb0f6040b7a120d2cee50db833ad
Søren Roed Schack <soer622t@edu.easj.dk>
2017-09-30 13:32:51 +0200

    Tilføjet en Customer klasse som matcher Customer.cs på min Provider : RestCustomerService. Oprettet en Task i main, der henter alle customers på listen.

---
+++ RestCustomerClientConsole/Customer.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Sockets;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace RestCustomerClientConsole
+{
+    /// <summary>
+    /// Uge 39 på Peters Semesterplan: http://pele-easj.dk/2017e-tek/SEMESTER.html
+    /// Link til opgaven: http://pele-easj.dk/2017e-tek/exercises/RestWCFService.docx
+    /// </summary>
+    public class Customer
+    {
+        [DataMember]
+        public int Id { get; set; }
+
+        [DataMember]
+        public string FirstName { get; set; }
+
+        [DataMember]
+        public string LastName { get; set; }
+
+        [DataMember]
+        public int Year { get; set; }
+
+        public Customer(int id, string first, string last, int year)
+        {
+            this.Id = id;
+            this.FirstName = first;
+            this.LastName = last;
+            this.Year = year;
+        }
+
+        public Customer()
+        {
+
+        }
+
+        //private static string CustomersUri = "http://localhost:60422/CustomerService.svc/customers/";
+
+        //private static async Task<IList<Customer>> GetCustomersAsync()
+        //{
+        //    using (HttpClient client = new HttpClient())
+        //    {
+        //        string content = await client.GetStringAsync(CustomersUri);
+        //        IList<Customer> cList = JsonConvert.DeserializeObject<IList<Customer>>(content);
+        //        return cList;
+        //    }
+        //}
+
+        //public void Start()
+        //{
+        //    var list = Task.Run(async () => await GetCustomersAsync());
+
+        //    foreach (var cust in list.Result)
+        //    {
+        //        Console.WriteLine(cust);
+        //    }
+        //} 
+
+
+        public override string ToString()
+        {
+            return Id + " " + FirstName + " " + LastName + " " + Year + " ";
+        }
+    }
+}
--- RestCustomerClientConsole/Program.cs
+++ RestCustomerClientConsole/Program.cs
@@ -1,15 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace RestCustomerClientConsole
 {
     class Program
     {
+        /// <summary>
+        /// Uge 39 på Peters Semesterplan: http://pele-easj.dk/2017e-tek/SEMESTER.html
+        /// Link til opgaven: http://pele-easj.dk/2017e-tek/exercises/RestWCFService.docx
+        /// </summary>
+
+        private static string CustomersUri = "http://localhost:60422/CustomerService.svc/customers/";
+
+        private static async Task<IList<Customer>> GetCustomersAsync()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                string content = await client.GetStringAsync(CustomersUri);
+                IList<Customer> cList = JsonConvert.DeserializeObject<IList<Customer>>(content);
+                return cList;
+            }
+        }
+
         static void Main(string[] args)
         {
+            var list = Task.Run(async () => await GetCustomersAsync());
+
+            foreach (var cust in list.Result)
+            {
+                Console.WriteLine(cust);
+            }
         }
     }
 }
